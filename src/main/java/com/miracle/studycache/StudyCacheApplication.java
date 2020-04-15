58ef85301f8bd092cfd92557869742126f27568c
LMiracle <k22227011@outlook.com>
2020-04-15 23:18:46 +0800

    init project

---
+++ src/main/java/com/miracle/studycache/StudyCacheApplication.java
@@ -0,0 +1,13 @@
+package com.miracle.studycache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCacheApplication.class, args);
+    }
+
+}
